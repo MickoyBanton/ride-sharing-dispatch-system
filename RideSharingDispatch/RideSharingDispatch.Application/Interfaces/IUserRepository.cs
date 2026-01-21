4bbe0f0b2ff3b8e66ddfca72cfb9df18507fae39
Mickoy Banton <mickoybanton11@gmail.com>
2026-01-21 00:10:18 -0500

    Add IUserRepository

---
+++ RideSharingDispatch/RideSharingDispatch.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using RideSharingDispatch.Domain.Entities;
+using RideSharingDispatch.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharingDispatch.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+    }
+}
