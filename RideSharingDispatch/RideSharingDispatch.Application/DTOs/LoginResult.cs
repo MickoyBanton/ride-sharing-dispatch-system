43cabef82e2c1b248760b6aabb4e9ae5e8ed54b5
Mickoy Banton <mickoybanton11@gmail.com>
2026-01-21 00:09:36 -0500

    Add LoginResult DTO

---
+++ RideSharingDispatch/RideSharingDispatch.Application/DTOs/LoginResult.cs
@@ -0,0 +1,17 @@
+﻿using RideSharingDispatch.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharingDispatch.Application.DTOs
+{
+    public class LoginResult
+    {
+        public bool IsSuccessful { get; set; }
+        public int? UserId { get; set; }
+        public UserRole? Role { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
