d1607a63645507d5dc038c112123146415858da6
Mickoy Banton <mickoybanton11@gmail.com>
2026-01-22 23:17:18 -0500

    Added token response DTO

---
+++ RideSharingDispatch/RideSharingDispatch.Application/DTOs/TokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharingDispatch.Application.DTOs
+{
+    public class TokenResponse
+    {
+        public DateTime ExpiresAt;
+        public string? Token;
+    }
+}
