abfb9db33b5a3d61a8f6e7d62c82bd4bf5dd836f
Mickoy Banton <mickoybanton11@gmail.com>
2026-01-25 21:44:02 -0500

    Added new DTOs for driver and rider registration request

---
+++ RideSharingDispatch/RideSharingDispatch.Application/DTOs/RegisterDriverRequest.cs
@@ -0,0 +1,25 @@
+﻿using RideSharingDispatch.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharingDispatch.Application.DTOs
+{
+    public class RegisterDriverRequest
+    {
+        public string Password { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public bool IsOnline { get; set; }
+
+        public decimal CurrentLatitude { get; set; }
+        public decimal CurrentLongitude { get; set; }
+
+        public VehicleType VehicleType { get; set; }
+    }
+}
