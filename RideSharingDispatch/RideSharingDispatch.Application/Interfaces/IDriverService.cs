77e5c0ac1d556b4e2884e1a5a31d3a9da88e4414
Mickoy Banton <mickoybanton11@gmail.com>
2026-01-26 15:41:59 -0500

    Added interface for DriverService

---
+++ RideSharingDispatch/RideSharingDispatch.Application/Interfaces/IDriverService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharingDispatch.Application.Interfaces
+{
+    public interface IDriverService
+    {
+        Task<bool> UpdateLocation(decimal latitude, decimal longitude, int userId);
+        Task<bool> SetOnlineStatus(bool isOnline, int userId);
+
+    }
+}
