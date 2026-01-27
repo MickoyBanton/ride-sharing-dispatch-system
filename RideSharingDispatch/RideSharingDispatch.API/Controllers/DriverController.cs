6724acdd95adfec6fc13a5827a5cad93e4a19d20
Mickoy Banton <mickoybanton11@gmail.com>
2026-01-26 20:30:38 -0500

    Implemented tripController and dirverController

--- RideSharingDispatch/RideSharingDispatch.API/Controllers/DriverController.cs
+++ RideSharingDispatch/RideSharingDispatch.API/Controllers/DriverController.cs
@@ -1,9 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RideSharingDispatch.Application.DTOs;
+using RideSharingDispatch.Application.Interfaces;
 
 namespace RideSharingDispatch.API.Controllers
 {
+    [Authorize(Roles = "Driver")]
     public class DriverController : ControllerBase
     {
-        
+        private readonly IDriverService _driverService;
+        private readonly ITripService _tripService;
+
+        public DriverController(IDriverService driverService, ITripService tripService)
+        {
+            this._driverService = driverService;
+            _tripService = tripService;
+        }
+
+        [HttpPut("{driverId}/status")]
+        public async Task<IActionResult> UpdateStatus(int driverId, UpdateDriverStatusRequest request)
+        {
+            bool result = await _driverService.SetOnlineStatus(request.IsOnline, driverId);
+
+            if (result == false)
+            {
+                return BadRequest("Status Change failed");
+            }
+            
+            return Ok();
+        }
+
+        [HttpPut("{driverId}/location")]
+        public async Task<IActionResult> UpdateLocation(int driverId, UpdateDriverLocationRequest request)
+        {
+            bool result = await _driverService.UpdateLocation(
+                request.Latitude,
+                request.Longitude,
+                driverId
+            );
+
+            if (result == false)
+            {
+                return BadRequest("Location Change failed");
+            }
+
+            return Ok(result);
+        }
+
+
+
     }
 }
--- RideSharingDispatch/RideSharingDispatch.API/Controllers/TripController.cs
+++ RideSharingDispatch/RideSharingDispatch.API/Controllers/TripController.cs
@@ -1,12 +1,96 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RideSharingDispatch.Application.DTOs;
+using RideSharingDispatch.Application.Interfaces;
+using RideSharingDispatch.Domain.Entities;
+using RideSharingDispatch.Domain.Enums;
 
 namespace RideSharingDispatch.API.Controllers
 {
-    public class TripController : Controller
+    [Authorize]
+    public class TripController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ITripService _tripService;
+        private readonly IDriverService _driverService;
+
+        public TripController(ITripService tripService, IDriverService driverService)
         {
-            return View();
+            _tripService = tripService;
+            _driverService = driverService;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTrip(CreateTripRequest request)
+        {
+            var trip = new Trip
+            {
+                RiderId = request.RiderId,
+                PickupLatitude = request.PickupLatitude,
+                PickupLongitude = request.PickupLongitude,
+                DestinationLatitude = request.DestinationLatitude,
+                DestinationLongitude = request.DestinationLongitude,
+                VehicleType = request.VehicleType,
+                TripStatus = TripStatus.Requested
+            };
+
+            await _tripService.CreateTrip(trip);
+
+            return Ok(trip);
+        }
+
+        [HttpGet("{tripId}")]
+        public async Task<IActionResult> GetTrip(int tripId)
+        {
+            var trip = await _tripService.GetTrip(tripId);
+
+            if (trip == null)
+                return NotFound();
+
+            return Ok(trip);
+        }
+
+        [HttpPost("{tripId}/cancel")]
+        public async Task<IActionResult> CancelTrip(int tripId)
+        {
+            var result = await _tripService.CancelTrip(tripId);
+            return Ok("Trip cancelled");
+        }
+
+        [HttpPost("trips/{tripId}/accept")]
+        public async Task<IActionResult> AcceptTrip(int tripId)
+        {
+            bool result = await _tripService.AssignDriver(tripId);
+            if (result == false)
+            {
+                return BadRequest();
+            }
+            return Ok("Trip accepted");
+        }
+
+
+
+        [HttpPost("{tripId}/arrived")]
+        public async Task<IActionResult> Arrived(int tripId)
+        {
+            var success = await _tripService.UpdateTripStatus(tripId, TripStatus.Arrived);
+            return success ? Ok() : BadRequest();
+        }
+
+        [HttpPost("{tripId}/start")]
+        public async Task<IActionResult> StartTrip(int tripId)
+        {
+            var success = await _tripService.UpdateTripStatus(tripId, TripStatus.InProgress);
+            return success ? Ok() : BadRequest();
+        }
+
+        [HttpPost("{tripId}/complete")]
+        public async Task<IActionResult> CompleteTrip(int tripId)
+        {
+            var success = await _tripService.CompleteTrip(tripId);
+            return success ? Ok() : BadRequest();
+        }
+
+
+       
     }
 }
